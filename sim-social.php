497e848744d95704c9188bc8d4c4a3f6edef2b1f
devuri <devuriko@gmail.com>
2020-07-12 22:35:07 -0500

    add schedule twicedaily

--- sim-social.php
+++ sim-social.php
@@ -11,7 +11,7 @@
  * @wordpress-plugin
  * Plugin Name:       Sim Social Feed
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
- * Description:       Easily Display Social Media Photo Feed for Instagram.
+ * Description:       Easily Display Social Media Photo Feed for Instagram. The feed will schedule twicedaily updates, you can also update manually with a single click.
  * Version:           1.2.5
  * Requires at least: 3.4
  * Requires PHP:      5.6
@@ -27,6 +27,32 @@
     if ( ! defined( 'WPINC' ) ) {
       die;
     }
+
+    /**
+     * setup activation schedule twicedaily
+     */
+    register_activation_hook( __FILE__, 'sim_social_twicedaily_schedule' );
+
+    /**
+     * check if the event is already scheduled
+     * if not setup our scheduled event
+     * wp_next_scheduled
+     * @link https://developer.wordpress.org/reference/functions/wp_next_scheduled/
+     */
+    function sim_social_twicedaily_schedule(){
+      if( false === wp_next_scheduled( 'sim_social_feed_twicedaily' ) ){
+        wp_schedule_event( time(), 'twicedaily', 'sim_social_feed_twicedaily' );
+      }
+    }
+
+    /**
+     * remove the scheduled event
+     */
+    register_deactivation_hook( __FILE__, 'remove_sim_social_twicedaily' );
+    function remove_sim_social_twicedaily(){
+      wp_clear_scheduled_hook( 'sim_social_feed_twicedaily' );
+    }
+
 
     /**
      * Load composer
@@ -54,6 +80,11 @@
      *  require_once // Load the main class.
      */
     require_once plugin_dir_path( __FILE__ ) . '/src/class-sim-social-feed.php';
+
+    /**
+     *  require_once // schedule.
+     */
+    require_once plugin_dir_path( __FILE__ ) . '/src/schedule.php';
 
     /**
      *  require_once // shortcode.
