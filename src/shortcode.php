cb3c23a365cd576eb58addb4583faed9af9d06e4
devuri <devuriko@gmail.com>
2020-07-23 14:12:32 -0500

    add shortcode option "limit" [ig_socialfeed limit="6"] default value is 6

--- src/shortcode.php
+++ src/shortcode.php
@@ -3,10 +3,17 @@
 	use SimIG\Instagram_Social\SimSocialFeed;
 
 	/**
-	 * shortcode to use [ig_socialfeed]
+	 * shortcode to use [ig_socialfeed limit="6"]
 	 */
 	add_shortcode('ig_socialfeed', 'simsf_igmedia_feed');
-	function simsf_igmedia_feed() {
+	function simsf_igmedia_feed($atts) {
+
+		$a = shortcode_atts(array(
+				'limit' => 6,
+		), $atts);
+
+		// params
+		$limit = $a['limit'];
 
 		/**
 		 * load the grid styles
@@ -14,7 +21,7 @@
 		wp_enqueue_style('sim-social-feed-grid');
 
 		ob_start();
-		SimSocialFeed::igfeed();
+		SimSocialFeed::igfeed($limit);
 		$output_sfd = ob_get_contents();
 		ob_end_clean();
 		return $output_sfd;
