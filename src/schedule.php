0463666f359af91aa77258f3a0feaf50c53ecee0
devuri <devuriko@gmail.com>
2020-11-30 12:12:38 -0500

    refactor: phpcs

--- src/schedule.php
+++ src/schedule.php
@@ -3,15 +3,15 @@
 	use SimSocialFeed\InstagramSocialFeed;
 
 	/**
-	 * run the sim social feed update
+	 * Run the sim social feed update
 	 */
 	add_action( 'sim_social_feed_cron', 'sim_social_igfeed_update' );
-	function sim_social_igfeed_update(){
+	function sim_social_igfeed_update() {
 		/**
-		 * update user media
+		 * Update user media
 		 */
 		if ( InstagramSocialFeed::is_request_ok() ) :
 			$ig_user_media = InstagramSocialFeed::user_media();
-			update_option('simsf_user_media', $ig_user_media->data );
+			update_option( 'simsf_user_media', $ig_user_media->data );
 		endif;
 	}
