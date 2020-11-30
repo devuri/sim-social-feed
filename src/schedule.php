47b238ff6288cc6318586cf7d6617613f739dce6
devuri <devuriko@gmail.com>
2020-11-30 12:21:52 -0500

    refactor: phpcs fix

--- src/schedule.php
+++ src/schedule.php
@@ -5,7 +5,6 @@
 	/**
 	 * Run the sim social feed update
 	 */
-	add_action( 'sim_social_feed_cron', 'sim_social_igfeed_update' );
 	function sim_social_igfeed_update() {
 		/**
 		 * Update user media
@@ -15,3 +14,4 @@
 			update_option( 'simsf_user_media', $ig_user_media->data );
 		endif;
 	}
+	add_action( 'sim_social_feed_cron', 'sim_social_igfeed_update' );
