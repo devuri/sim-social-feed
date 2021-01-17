96540ad333d2406e2d65e6be827f2550ca5c7f92
devuri <devuriko@gmail.com>
2021-01-17 01:11:49 -0500

    2.7.2

--- sim-social.php
+++ sim-social.php
@@ -12,7 +12,7 @@
  * Plugin Name:       Sim Social Feed
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
  * Description:       Easily Display Social Media Photo Feed for Instagram. The feed will schedule twicedaily updates, you can also update manually with a single click.
- * Version:           2.7.1
+ * Version:           2.7.2
  * Requires at least: 3.4
  * Requires PHP:      5.6
  * Author:            SwitchWebdev.com
